13b39d6b91c462bc668e64408d940649debaa224
James Martineau <James.Martineau@libertymutual.com>
2017-12-18 22:12:13 -0500

    client post now working and adding to friend.js

--- app/routes/apiRoutes/apiRoutes.js
+++ app/routes/apiRoutes/apiRoutes.js
@@ -6,7 +6,9 @@ var friendList = require('../../data/friends.js');
 var router = express.Router();
 
 var bodyParser = require("body-parser");
-var parseUrlEncoded = bodyParser.urlencoded({ extended: false });
+var parseUrlEncoded = bodyParser.urlencoded({
+    extended: false
+});
 
 
 
@@ -20,23 +22,23 @@ router.route('/')
         // res.send('WELCOME TO THE SURVEY PAGE')
         //res.sendFile(path.join(__dirname, "../../public/survey.html"));
 
+        //declare and assign object posted from client
         var newFriend = req.body;
-        // var newFriendScores=[];
-        console.log(newFriend);
 
-        // for(var i = 0; i < newFriend.scores.length; i++){
-        //     console.log(newFriend.scores[i]);
-        // }
-        // newFriend.scores.forEach(element => {
-        //     newFriendScores.push(parseInt(element));
-        // });
+        //declare new variable to convert to array
+        var newFriendScores = [];
 
-        // newFriend.scores = newFriendScores;
+        //loop through score string, convert to Integer, add to array
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            newFriendScores.push(parseInt(newFriend.scores[i]));
+        }
 
-        // console.log(req.body); 
-      
-        // friendList.push(newFriend);
-      
+        //overwrite string values with new array
+        newFriend.scores = newFriendScores;
+
+        //add newFriend object to friend.js file
+        friendList.push(newFriend);
+
         res.send("success");
     })
 
